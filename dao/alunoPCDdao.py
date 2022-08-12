ba7cc9019e545eca4105cf45e0b11ae94f217f7e
dev-welligtonlins <welligtonlinsemp@gmail.com>
2022-08-12 13:50:16 -0500

    Criação do user interprete e modelo alunoDAO

---
+++ dao/alunoPCDdao.py
@@ -0,0 +1,16 @@
+from entidades.alunoPCD import AlunoPCD
+
+class AlunoDao:
+
+    def __init__(self):
+        self._alunosPCD = []
+
+    def criaAlunoPCD(self, alunoPcd):
+        self._alunosPCD.append(alunoPcd)
+
+
+    def listar(self):
+        for aluno in self._alunosPCD:
+            print(f'Nome: {aluno.nome}')
+            print(f'Matricula: {aluno.matricula}')
+            print(f'Cpf: {aluno.cpf}')
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from entidades.alunoPCD import AlunoPCD
 from entidades.interprete import Interprete
-aluno = AlunoPCD(1, "nome", "cpf", "email", "telefone", "matricula", "curso",
+from dao.alunoPCDdao import AlunoDao
+
+alunosDao = AlunoDao()
+
+aluno = AlunoPCD(1, "carlos", "333.444.111-00", "email", "telefone", "20220400006", "S.I",
                  "disciplinas", "periodo", "deficiencia", "data_nascimento")
 
-print(f'{aluno} wrwerv wer {aluno.id}')
-
+alunosDao.criaAlunoPCD(aluno)
+alunosDao.listar()
 interprete = Interprete(2, "n1", "cpf", "email", "telefone")
 
-print(interprete.nome)
-interprete.alteraNome("Correto....")
-print(interprete.nome)
