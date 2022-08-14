3b3c2b7c3202d91c58019add80ccc13012d155e8
brunofernandoacosta <brunofernandodacgmail.com>
2022-08-13 19:19:57 -0500

    classe curso

---
+++ entidades/Curso.py
@@ -0,0 +1,21 @@
+class Cursos:
+
+    def __init__(self, id, nome):
+        self._id = id
+        self._nome = nome
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
