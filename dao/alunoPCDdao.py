431923f16ac321e59acb29267e01334d677f6a13
ArthurOliveira173 <arthuroliveiradevasconcelos@gmail.com>
2022-08-17 15:44:03 -0500

    Adicionando alunoPCD no banco

--- dao/alunoPCDdao.py
+++ dao/alunoPCDdao.py
@@ -4,12 +4,27 @@ from conexao.conexaoBD import ConexaoBD
 import mysql.connector
 
 class AlunoDao:
-
+    _conexao = ConexaoBD.criarConexao("root", "tesi1")
     def __init__(self):
-        self._alunosPcd = []
+        pass
 
-    def AdicionarAlunoPcd(self, alunoPcd):
-        self._alunosPcd.append(alunoPcd)
+    @property
+    def conexao(self):
+        return self._conexao
+
+    @conexao.setter
+    def conexao(self, conexao):
+        self._conexao = conexao
+
+    def AdicionarAlunoPcd(self):
+
+        cursor = self._conexao.cursor()
+
+        sql = "INSERT INTO aluno_pcd (alu_id, alu_nome, alu_cpf, alu_sexo, alu_email, alu_telefone, alu_matricula, alu_deficiencias, alu_Periodo_Academico, alu_data_nascimento) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (4, "John", "asd", "M", "asd", "asd", "asd", "asd", "asd", "2000-08-08")
+        cursor.execute(sql, val)
+
+        self._conexao.commit()
 
     def removeAlunoPcd(self, alunoPcd):
         self._alunosPcd.remove(alunoPcd)
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from entidades.alunoPcd import AlunoPcd
 from entidades.interprete import Interprete
 from dao.alunoPCDdao import AlunoDao
 from entidades.avisos import Avisos
+from conexao.conexaoBD import ConexaoBD
 
-alunosDao = AlunoDao()
+#cnx = ConexaoBD.criarConexao("root", "tesi1")
+alunoDao = AlunoDao()
+alunoDao.AdicionarAlunoPcd()
+
+# alunosDao = AlunoDao()
 
 aluno = AlunoPcd(1, "carlos", "333.444.111-00", "email", "telefone", "20220400006", "S.I",
                  "disciplinas", "periodo", "deficiencia", "data_nascimento")
@@ -11,14 +16,16 @@ aluno = AlunoPcd(1, "carlos", "333.444.111-00", "email", "telefone", "2022040000
 aluno2 = AlunoPcd(2, "manoel", "333.444.111-00", "email", "telefone", "20220400006", "S.I",
                  "disciplinas", "periodo", "deficiencia", "data_nascimento")
 
-avisos = Avisos(2, "casa", "porta janela", "28-05-2021", 1)
 
-print("id do aviso:", avisos.id)
 
-alunosDao.AdicionarAlunoPcd(aluno)
-alunosDao.AdicionarAlunoPcd(aluno2)
-alunosDao.removeAlunoPcd(aluno)
-
-alunosDao.listar()
+# avisos = Avisos(2, "casa", "porta janela", "28-05-2021", 1)
+#
+# print("id do aviso:", avisos.id)
+#
+# alunosDao.AdicionarAlunoPcd(aluno)
+# alunosDao.AdicionarAlunoPcd(aluno2)
+# alunosDao.removeAlunoPcd(aluno)
+#
+# alunosDao.listar()
 interprete = Interprete(2, "n1", "cpf", "email", "telefone")
 
