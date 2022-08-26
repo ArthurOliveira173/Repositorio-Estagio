173328cf6b7aed3e0d167db9f340e829857f7f79
brunofernandoacosta <brunofernandodacgmail.com>
2022-08-26 15:40:45 -0500

    Listar curso, todos e especifico

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from entidades.alunoPcd import AlunoPcd
 from entidades.Curso import Cursos
 from dao.alunoDao import AlunoDao
+from dao.cursoDao import CursoDao
 
 # alunoDao = AlunoDao()
 # aluno1 = (8, "John", "asd", "M", "asd", "asd", "asd", "asd", "asd", "2000-08-08")
@@ -11,8 +12,18 @@ from dao.alunoDao import AlunoDao
 # for aluno in lista:
 #     print(aluno)
 
-curso1 = Cursos.CriarCurso(1, "Libras", 8)
+cursoDao = CursoDao()
 
+# curso2 = Cursos.CriarCurso(2, "sistema de inforamção", 8)
+# curso3 = Cursos.CriarCurso(3, "Letras Libras", 8)
+
+lista = cursoDao.listarTudoCurso()
+lista2 = cursoDao.listarCurso("cur_id", 3)
+for curso in lista:
+     print(curso)
+print()
+for curso in lista2:
+     print(curso)
 
 # aluno = AlunoPcd.CriarAlunoPcd(11, "carlos", "33344411100", "M", "email", "telefone", "20220400006",
 #                                "deficiencia", "periodo", "2000-08-08")
