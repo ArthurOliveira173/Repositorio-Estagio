cd1d4de351974f081ea7ea2f7e580fc4eb19cff2
welligtonlins <welligtonlinsufac@gmail.com>
2022-09-23 13:38:50 -0500

    #2 templates de avisos e conecção sql no settings

--- avisos/models.py
+++ avisos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+class Avisos(models.Model):
+    avi_id = models.IntegerField(primary_key=True)
+    avi_titulo = models.CharField(max_length=255)
+    avi_descricao = models.CharField(max_length=255, blank=True, null=True)
+    avi_data = models.DateTimeField()
+    avi_arquivos = models.TextField(blank=True, null=True)
