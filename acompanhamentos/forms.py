e195a8a11800e0d7524aa87f46b97b3b846122b2
ArthurOliveira173 <arthuroliveiradevasconcelos@gmail.com>
2022-10-25 17:27:54 -0500

    Criando acompanhamentos de disciplina, monitor, tutor e interprete

--- acompanhamentos/forms.py
+++ acompanhamentos/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Acompanhamentos
+from .models import Acompanhamentos, AcompanhamentoDisciplinas, AcompanhamentoMonitores, AcompanhamentoTutores, AcompanhamentoInterpretes
 
 class AcompanhamentosForm(ModelForm):
 
@@ -17,4 +17,73 @@ class AcompanhamentosForm(ModelForm):
             'aco_semestre': forms.TextInput(attrs={'class':'form-control', 'placeholder': '____._'}),
             'aco_inicio': forms.DateInput(attrs={'class':'form-control'}),
             'aco_fim': forms.DateInput(attrs={'class':'form-control'}),
+            'aco_aluno_pcd': forms.Select(attrs={'class':'form-control'}),
+        }
+
+class AcoDisciplinasForm(ModelForm):
+
+    class Meta:
+        model = AcompanhamentoDisciplinas
+        fields = ('AsDis_disciplina', 'AsDis_acompanhamento')
+        labels = {
+            'AsDis_disciplina': 'Selecione a disciplina',
+            'AsDis_acompanhamento': 'Defina o acompanhamento relacionado',
+        }
+        widgets = {
+            'AsDis_disciplina': forms.Select(attrs={'class': 'form-control'}),
+            'AsDis_acompanhamento': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+class AcoMonitoriasForm(ModelForm):
+
+    class Meta:
+        model = AcompanhamentoMonitores
+        fields = ('AsMon_inicio', 'AsMon_fim', 'AsMon_monitor', 'AsMon_acompanhamento')
+        labels = {
+            'AsMon_inicio': 'Defina a data inicial da monitoria',
+            'AsMon_fim': 'Defina a data final da monitoria',
+            'AsMon_monitor': 'Defina o monitor',
+            'AsMon_acompanhamento': 'Defina o acompanhamento relacionado',
+        }
+        widgets = {
+            'AsMon_inicio': forms.DateInput(attrs={'class':'form-control'}),
+            'AsMon_fim': forms.DateInput(attrs={'class':'form-control'}),
+            'AsMon_monitor': forms.Select(attrs={'class': 'form-control'}),
+            'AsMon_acompanhamento': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+class AcoTutoriasForm(ModelForm):
+
+    class Meta:
+        model = AcompanhamentoTutores
+        fields = ('AsTut_inicio', 'AsTut_fim', 'AsTut_tutor', 'AsTut_acompanhamento')
+        labels = {
+            'AsTut_inicio': 'Defina a data inicial da tutoria',
+            'AsTut_fim': 'Defina a data final da tutoria',
+            'AsTut_tutor': 'Defina o tutor',
+            'AsTut_acompanhamento': 'Defina o acompanhamento relacionado',
+        }
+        widgets = {
+            'AsTut_inicio': forms.DateInput(attrs={'class':'form-control'}),
+            'AsTut_fim': forms.DateInput(attrs={'class':'form-control'}),
+            'AsTut_tutor': forms.Select(attrs={'class': 'form-control'}),
+            'AsTut_acompanhamento': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+class AcoInterpretacoesForm(ModelForm):
+
+    class Meta:
+        model = AcompanhamentoInterpretes
+        fields = ('AsInt_inicio', 'AsInt_fim', 'AsInt_interprete', 'AsInt_acompanhamento')
+        labels = {
+            'AsInt_inicio': 'Defina a data inicial da interpretação',
+            'AsInt_fim': 'Defina a data final da interpretação',
+            'AsInt_interprete': 'Defina o interprete',
+            'AsInt_acompanhamento': 'Defina o acompanhamento relacionado',
+        }
+        widgets = {
+            'AsInt_inicio': forms.DateInput(attrs={'class':'form-control'}),
+            'AsInt_fim': forms.DateInput(attrs={'class':'form-control'}),
+            'AsInt_interprete': forms.Select(attrs={'class': 'form-control'}),
+            'AsInt_acompanhamento': forms.Select(attrs={'class': 'form-control'}),
         }
--- acompanhamentos/models.py
+++ acompanhamentos/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.utils import timezone
 from membros.models import AlunoPcd, Monitor, Tutor, Interprete
