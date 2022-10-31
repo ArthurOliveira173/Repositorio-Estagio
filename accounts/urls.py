95b74882e76349725edbef6e3730c39e2892ea96
welligtonlins <welligtonlinsufac@gmail.com>
2022-10-31 20:58:36 -0300

    confirmação de senha no email

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,8 +16,9 @@ urlpatterns = [
     path('dashboardAluno/', views.dashboardAluno, name='dashboardAluno'),
 
     #template_name="accounts/reset_password.html"
+    #template_name="accounts/password_reset_done.html"
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/reset_password.html"), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_done"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/reset_password_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), name="password_reset_complete"),
 
