af1b9b9052e9a9389ab5ffa56a6a1016a1a384a4
welligtonlins <welligtonlinsufac@gmail.com>
2022-10-31 17:33:33 -0300

    BaseLog em Accounts templates static

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('cadastroTutor', views.cadastroTutor, name='cadastroTutor'),
     path('dashboardAluno/', views.dashboardAluno, name='dashboardAluno'),
 
+    #template_name="accounts/reset_password.html"
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/reset_password.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name="password_reset_confirm"),
