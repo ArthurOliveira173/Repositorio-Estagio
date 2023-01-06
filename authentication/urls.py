cb02e79210cd1024f556892109066551f8c6256a
ArthurOliveira173 <arthuroliveiradevasconcelos@gmail.com>
2023-01-06 17:10:14 -0500

    Cadastro de monitor, tutor e interprete

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('authLogin', views.authLogin, name='authLogin'),
     path('authLogout', views.authLogout, name='authLogout'),
     path('authRegister', views.authRegister, name='authRegister'),
+    path('authRegisterGeral', views.authRegisterGeral, name='authRegisterGeral'),
     path('authRegisterAluno', views.authRegisterAluno, name='authRegisterAluno'),
+    path('authRegisterMonitor', views.authRegisterMonitor, name='authRegisterMonitor'),
+    path('authRegisterTutor', views.authRegisterTutor, name='authRegisterTutor'),
+    path('authRegisterInterprete', views.authRegisterInterprete, name='authRegisterInterprete')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -5,16 +5,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import validate_email
 from string import ascii_letters
 from django.contrib import messages
-from membros.forms import AlunosForm
+from membros.forms import AlunosForm, MonitoresForm, TutoresForm, InterpretesForm
 
 def authLogin(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        user = authenticate(request, username=usuario, password=senha)
         if user is not None:
             login(request, user)
-            messages.success('Login realizado com sucesso!')
+            messages.success(request, 'Login realizado com sucesso!')
             return redirect('aviIndex')
         else:
             messages.error(request, 'usuário ou senha inválidos!')
@@ -50,29 +51,33 @@ def authRegister(request):
         'form': form,
     })
 
+def authRegisterGeral(request):
+    return render(request, 'authenticate/authRegisterGeral.html', {
+
+    })
+
 def authRegisterAluno(request):
     if request.method == "POST":
         form = AlunosForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
             nome = request.POST.get('alu_nome')
             usuario = request.POST.get('alu_cpf')
             data_nascimento = request.POST.get('alu_data_nascimento')
-            sexo = request.POST.get('alu_genero')
+            genero = request.POST.get('alu_genero')
             email_pessoal = request.POST.get('alu_email_pessoal')
             email_instituicao = request.POST.get('alu_email_institucional')
             telefone = request.POST.get('alu_telefone')
             cep = request.POST.get('alu_endereco_cep')
-            descricao_des = request.POST.get('alu_endereco_descricao')
+            endereco_des = request.POST.get('alu_endereco_descricao')
             cidade = request.POST.get('alu_endereco_cidade')
-            curso = request.POST.get(' alu_curso')
+            curso = request.POST.get('alu_curso')
             periodo = request.POST.get('alu_periodo_academico')
             matricula = request.POST.get('alu_matricula')
             deficiencias = request.POST.get('alu_deficiencias')
             senha = request.POST.get('senha')
             senha2 = request.POST.get('senha2')
 
-            if not nome or not usuario or not data_nascimento or not sexo or not email_pessoal or not email_instituicao or not telefone or not cep or not descricao_des or not cidade or not periodo or not matricula or not senha or not senha2:
+            if not nome or not usuario or not data_nascimento or not genero or not email_pessoal or not email_instituicao or not telefone or not cep or not endereco_des or not cidade or not curso or not periodo or not matricula or not senha or not senha2:
                 messages.error(request, 'Todos os campos devem ser preenchidos!')
                 return render(request, 'authenticate/authRegisterAluno.html', {'form': form})
 
@@ -143,64 +148,259 @@ def authRegisterAluno(request):
 
 def authRegisterMonitor(request):
     if request.method == "POST":
-        form = AlunosForm(request.POST)
+        form = MonitoresForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                messages.success(request, "Cadastrado com sucesso!")
-                return redirect('authLogin')
-            else:
-                messages.error("Erro na tentativa de cadastro.")
-                return redirect('authLogin')
-    else:
-        form = AlunosForm()
+            nome = request.POST.get('mon_nome')
+            usuario = request.POST.get('mon_cpf')
+            genero = request.POST.get('mon_genero')
+            email_pessoal = request.POST.get('mon_email_pessoal')
+            email_instituicao = request.POST.get('mon_email_institucional')
+            telefone = request.POST.get('mon_telefone')
+            cep = request.POST.get('mon_endereco_cep')
+            endereco_des = request.POST.get('mon_endereco_descricao')
+            cidade = request.POST.get('mon_endereco_cidade')
+            curso = request.POST.get('mon_curso')
+            periodo = request.POST.get('mon_periodo_academico')
+            matricula = request.POST.get('mon_matricula')
+            senha = request.POST.get('senha')
+            senha2 = request.POST.get('senha2')
 
-    return render(request, 'authenticate/authRegister.html', {
+            if not nome or not usuario or not genero or not email_pessoal or not email_instituicao or not telefone or not cep or not endereco_des or not cidade or not curso or not periodo or not matricula or not senha or not senha2:
+                messages.error(request, 'Todos os campos devem ser preenchidos!')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            try:
+                valida_string(nome)
+            except:
+                messages.error(request, 'Por favor digite somente letras e espaços')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            try:
+                validate_email(email_pessoal)
+            except:
+                messages.error(request, 'Email pessoal invalido!')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            try:
+                validate_email(email_instituicao)
+            except:
+                messages.error(request, 'Email institucional invalido!')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            try:
+                valida_cpf(usuario)
+            except:
+                messages.error(request, 'O Cpf informado não é valido, tente novamente!')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            if User.objects.filter(username=usuario).exists():
+                messages.error(request, 'Cpf já cadastrado, verifique e tente novamente!')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            if User.objects.filter(email=email_pessoal).exists():
+                messages.error(request, 'Email pessoal já cadastrado, tente novamente!')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            if User.objects.filter(email=email_instituicao).exists():
+                messages.error(request, 'Email institucional já cadastrado, tente novamente!')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            if len(senha) < 6:
+                messages.error(request, 'Senha precisa ter 7 caracteres ou mais.')
+                return render(request, 'authenticate/authRegisterMonitor.html', {'form': form})
+
+            if senha != senha2:
+                messages.error(request, 'Senhas diferentes, tente novamente!')
+                return render(request, 'authenticate/authRegisterMonitor.html')
+
+            user = User.objects.create_user(username=usuario, email=email_pessoal,
+                                            password=senha, first_name=nome,
+                                            last_name=email_instituicao)
+            if user is not None:
+                form.save()
+                user.save()
+                messages.success(request, 'Cadastro realizado com sucesso!')
+                return redirect("authLogin")
+            else:
+                messages.error("ocorreu um erro na tentativa de cadastro! Usuário não foi criado.")
+                return redirect("authLogin")
+        else:
+            messages.error(request, 'Informações invalidas, tente novamente!')
+            form = MonitoresForm(request.POST)
+    else:
+        form = MonitoresForm()
+
+    return render(request, 'authenticate/authRegisterMonitor.html', {
         'form': form,
     })
 
 def authRegisterTutor(request):
     if request.method == "POST":
-        form = AlunosForm(request.POST)
+        form = TutoresForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                messages.success(request, "Cadastrado com sucesso!")
-                return redirect('authLogin')
-            else:
-                messages.error("Erro na tentativa de cadastro.")
-                return redirect('authLogin')
-    else:
-        form = AlunosForm()
+            nome = request.POST.get('tut_nome')
+            usuario = request.POST.get('tut_cpf')
+            genero = request.POST.get('tut_genero')
+            email_pessoal = request.POST.get('tut_email_pessoal')
+            email_instituicao = request.POST.get('tut_email_institucional')
+            telefone = request.POST.get('tut_telefone')
+            cep = request.POST.get('tut_endereco_cep')
+            endereco_des = request.POST.get('tut_endereco_descricao')
+            cidade = request.POST.get('tut_endereco_cidade')
+            curso = request.POST.get('tut_curso')
+            periodo = request.POST.get('tut_periodo_academico')
+            matricula = request.POST.get('tut_matricula')
+            senha = request.POST.get('senha')
+            senha2 = request.POST.get('senha2')
 
-    return render(request, 'authenticate/authRegister.html', {
+            if not nome or not usuario or not genero or not email_pessoal or not email_instituicao or not telefone or not cep or not endereco_des or not cidade or not curso or not periodo or not matricula or not senha or not senha2:
+                messages.error(request, 'Todos os campos devem ser preenchidos!')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            try:
+                valida_string(nome)
+            except:
+                messages.error(request, 'Por favor digite somente letras e espaços')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            try:
+                validate_email(email_pessoal)
+            except:
+                messages.error(request, 'Email pessoal invalido!')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            try:
+                validate_email(email_instituicao)
+            except:
+                messages.error(request, 'Email institucional invalido!')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            try:
+                valida_cpf(usuario)
+            except:
+                messages.error(request, 'O Cpf informado não é valido, tente novamente!')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            if User.objects.filter(username=usuario).exists():
+                messages.error(request, 'Cpf já cadastrado, verifique e tente novamente!')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            if User.objects.filter(email=email_pessoal).exists():
+                messages.error(request, 'Email pessoal já cadastrado, tente novamente!')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            if User.objects.filter(email=email_instituicao).exists():
+                messages.error(request, 'Email institucional já cadastrado, tente novamente!')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            if len(senha) < 6:
+                messages.error(request, 'Senha precisa ter 7 caracteres ou mais.')
+                return render(request, 'authenticate/authRegisterTutor.html', {'form': form})
+
+            if senha != senha2:
+                messages.error(request, 'Senhas diferentes, tente novamente!')
+                return render(request, 'authenticate/authRegisterTutor.html')
+
+            user = User.objects.create_user(username=usuario, email=email_pessoal,
+                                            password=senha, first_name=nome,
+                                            last_name=email_instituicao)
+            if user is not None:
+                form.save()
+                user.save()
+                messages.success(request, 'Cadastro realizado com sucesso!')
+                return redirect("authLogin")
+            else:
+                messages.error("ocorreu um erro na tentativa de cadastro! Usuário não foi criado.")
+                return redirect("authLogin")
+        else:
+            messages.error(request, 'Informações invalidas, tente novamente!')
+            form = TutoresForm(request.POST)
+    else:
+        form = TutoresForm()
+
+    return render(request, 'authenticate/authRegisterTutor.html', {
         'form': form,
     })
 
 def authRegisterInterprete(request):
     if request.method == "POST":
-        form = AlunosForm(request.POST)
+        form = InterpretesForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                messages.success(request, "Cadastrado com sucesso!")
-                return redirect('authLogin')
-            else:
-                messages.error("Erro na tentativa de cadastro.")
-                return redirect('authLogin')
-    else:
-        form = AlunosForm()
+            nome = request.POST.get('int_nome')
+            usuario = request.POST.get('int_cpf')
+            sexo = request.POST.get('int_genero')
+            email_pessoal = request.POST.get('int_email_pessoal')
+            email_instituicao = request.POST.get('int_email_institucional')
+            telefone = request.POST.get('int_telefone')
+            senha = request.POST.get('senha')
+            senha2 = request.POST.get('senha2')
 
-    return render(request, 'authenticate/authRegister.html', {
+            if not nome or not usuario or not sexo or not email_pessoal or not email_instituicao or not telefone or not senha or not senha2:
+                messages.error(request, 'Todos os campos devem ser preenchidos!')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            try:
+                valida_string(nome)
+            except:
+                messages.error(request, 'Por favor digite somente letras e espaços')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            try:
+                validate_email(email_pessoal)
+            except:
+                messages.error(request, 'Email pessoal invalido!')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            try:
+                validate_email(email_instituicao)
+            except:
+                messages.error(request, 'Email institucional invalido!')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            try:
+                valida_cpf(usuario)
+            except:
+                messages.error(request, 'O Cpf informado não é valido, tente novamente!')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            if User.objects.filter(username=usuario).exists():
+                messages.error(request, 'Cpf já cadastrado, verifique e tente novamente!')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            if User.objects.filter(email=email_pessoal).exists():
+                messages.error(request, 'Email pessoal já cadastrado, tente novamente!')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            if User.objects.filter(email=email_instituicao).exists():
+                messages.error(request, 'Email institucional já cadastrado, tente novamente!')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            if len(senha) < 6:
+                messages.error(request, 'Senha precisa ter 7 caracteres ou mais.')
+                return render(request, 'authenticate/authRegisterInterprete.html', {'form': form})
+
+            if senha != senha2:
+                messages.error(request, 'Senhas diferentes, tente novamente!')
+                return render(request, 'authenticate/authRegisterInterprete.html')
+
+            user = User.objects.create_user(username=usuario, email=email_pessoal,
+                                            password=senha, first_name=nome,
+                                            last_name=email_instituicao)
+            if user is not None:
+                form.save()
+                user.save()
+                messages.success(request, 'Cadastro realizado com sucesso!')
+                return redirect("authLogin")
+            else:
+                messages.error("ocorreu um erro na tentativa de cadastro! Usuário não foi criado.")
+                return redirect("authLogin")
+        else:
+            messages.error(request, 'Informações invalidas, tente novamente!')
+            form = InterpretesForm(request.POST)
+    else:
+        form = InterpretesForm()
+
+    return render(request, 'authenticate/authRegisterInterprete.html', {
         'form': form,
     })
 
