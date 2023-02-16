84ecab4301defcf0bb7d3a101466f490f1d00be7
ArthurOliveira173 <arthuroliveiradevasconcelos@gmail.com>
2023-02-16 15:34:13 -0500

    Adicionando logica de permissao em todas as paginas que usam baseAdmin e baseAluno como template

--- portal/settings.py
+++ portal/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'estagio',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'tesi1',
         'HOST':'localhost',
         'PORT':'3306',
     }
