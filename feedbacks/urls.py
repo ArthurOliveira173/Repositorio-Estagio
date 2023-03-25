289d980313226464d9b9d3e161fc066c2cea327e
ArthurOliveira173 <arthuroliveiradevasconcelos@gmail.com>
2023-03-25 18:27:27 -0500

    Criando funcionalidade de feedbacks para todos os usuarios

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -8,11 +8,34 @@ urlpatterns = [
     path('feedback/<int:feedback_id>', views.feedback, name='feedback'),
     path('atualizarFeedback/<int:feedback_id>', views.atualizarFeedback, name='atualizarFeedback'),
 
+    path('feedback/baixarFileFeedback/<str:filename>/', views.baixarFileFeedback, name='baixarFileFeedback'),
+
+    #ADMIN
+    path('feedback/adminFeedbackAll', views.adminFeedbackAll, name='adminFeedbackAll'),
+    path('feedback/adminFeedbackAll/adminOpenfeedback/<int:feedback_id>', views.adminOpenfeedback, name='adminOpenfeedback'),
+    path('feedback/adminFeedbackAll/adminOpenAllfeedback/<int:feedback_id>', views.adminOpenAllfeedback, name='adminOpenAllfeedback'),
+    path('feedback/adminRespostaFeedback/<int:feedback_id>/', views.adminRespostaFeedback, name='adminRespostaFeedback'),
+    path('feedback/adminDeletarFeedback/<int:feedback_id>/', views.adminDeletarFeedback, name='adminDeletarFeedback'),
+
     #ALUNO
     path('feedback/alunoFeedback/<int:user_id>', views.alunoFeedback, name='alunoFeedback'),
     path('feedback/alunoFeedbackAll/<int:user_id>', views.alunoFeedbackAll, name='alunoFeedbackAll'),
     path('feedback/alunoFeedbackAll/<int:user_id>/alunoOpenfeedback/<int:feedback_id>', views.alunoOpenfeedback, name='alunoOpenfeedback'),
     path('feedback/alunoFeedbackAll/<int:user_id>/alunoOpenAllfeedback/<int:feedback_id>', views.alunoOpenAllfeedback, name='alunoOpenAllfeedback'),
     path('feedback/alunoRespostaFeedback/<int:user_id>/<int:feedback_id>/', views.alunoRespostaFeedback, name='alunoRespostaFeedback'),
+
+    #MONITOR
+    path('feedback/monitorFeedback/<int:user_id>', views.monitorFeedback, name='monitorFeedback'),
+    path('feedback/monitorFeedbackAll/<int:user_id>', views.monitorFeedbackAll, name='monitorFeedbackAll'),
+    path('feedback/monitorFeedbackAll/<int:user_id>/monitorOpenfeedback/<int:feedback_id>', views.monitorOpenfeedback, name='monitorOpenfeedback'),
+    path('feedback/monitorFeedbackAll/<int:user_id>/monitorOpenAllfeedback/<int:feedback_id>', views.monitorOpenAllfeedback, name='monitorOpenAllfeedback'),
+    path('feedback/monitorRespostaFeedback/<int:user_id>/<int:feedback_id>/', views.monitorRespostaFeedback, name='monitorRespostaFeedback'),
+
+    #TUTOR
+    path('feedback/tutorFeedback/<int:user_id>', views.tutorFeedback, name='tutorFeedback'),
+    path('feedback/tutorFeedbackAll/<int:user_id>', views.tutorFeedbackAll, name='tutorFeedbackAll'),
+    path('feedback/tutorFeedbackAll/<int:user_id>/tutorOpenfeedback/<int:feedback_id>', views.tutorOpenfeedback, name='tutorOpenfeedback'),
+    path('feedback/tutorFeedbackAll/<int:user_id>/tutorOpenAllfeedback/<int:feedback_id>', views.tutorOpenAllfeedback, name='tutorOpenAllfeedback'),
+    path('feedback/tutorRespostaFeedback/<int:user_id>/<int:feedback_id>/', views.tutorRespostaFeedback, name='tutorRespostaFeedback'),
 
 ]
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,13 +1,16 @@
+import os
+import mimetypes
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
+from django.http import HttpResponse
 from .models import Feedbacks
 from django.http import HttpResponseRedirect
 from django.db.models import Q
 from django.core.paginator import Paginator
-from .forms import FeedbacksForm, FeedbacksRespostaForm
+from django.contrib import messages
+from .forms import FeedbacksForm, FeedbacksRespostaForm, FeedbacksMonitorForm, FeedbacksTutorForm
 
-from membros.models import AlunoPcd, CustomUser
-from acompanhamentos.models import Acompanhamentos
+from membros.models import AlunoPcd, CustomUser, Monitor, Tutor
+from acompanhamentos.models import Acompanhamentos, AcompanhamentoMonitores, AcompanhamentoTutores
 
 # def handle_uploaded_file(f):
 #     with open('avisos/uploads/'+f.name, 'wb+') as destination:
@@ -86,7 +89,120 @@ def buscarFeedback(request):
 
         })
 
+def baixarFileFeedback(request, filename):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filename = str(filename)
+    filepath = BASE_DIR + '\\uploads\\' + filename
+    path = open(filepath, 'rb')
+    mime_type, _ = mimetypes.guess_type(filepath)
+    response = HttpResponse(path, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response
 
+#ADMIN-FEEDBACK
+def adminFeedbackAll(request):
+    feedback = Feedbacks.objects.order_by('-fee_id').filter(fee_anterior=None )
+
+    paginator = Paginator(feedback, 7)
+    page = request.GET.get('p')
+    feedback = paginator.get_page(page)
+
+    return render(request, 'feedbacks/adminFeedbackAll.html', {
+        'feedback': feedback,
+    })
+
+def adminOpenAllfeedback(request, feedback_id):
+    feedbackInicial = get_object_or_404(Feedbacks, fee_id=feedback_id)
+    list_feedback = Feedbacks.objects.all()
+
+    feedback = []
+    f0 = feedbackInicial
+    feedback.append(f0)
+    ultimo_feedback = None
+    for f1 in list_feedback:
+        if f1.fee_anterior == f0.fee_id:
+            feedback.append(f1)
+            f0=f1
+            if f1.fee_proximo == None:
+                ultimo_feedback = f1
+                break
+    feedback = reversed(feedback)
+    return render(request, 'feedbacks/adminOpenAllfeedback.html', {
+        'feedback': feedback,
+        'ultimo_feedback': ultimo_feedback,
+        'feedbackInicial': feedbackInicial
+    })
+def adminOpenfeedback(request, feedback_id):
+    feedback = get_object_or_404(Feedbacks, fee_id=feedback_id)
+
+    if feedback.fee_proximo != None:
+        return adminOpenAllfeedback(request, feedback_id)
+    return render(request, 'feedbacks/adminOpenfeedback.html', {
+        'feedback': feedback
+
+    })
+
+def adminRespostaFeedback(request, feedback_id):
+    submitted = False
+    context = {}
+
+    if request.POST:
+        form = FeedbacksRespostaForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            Feedbackform = form.save(commit=False)
+            ultimo_feedback = get_object_or_404(Feedbacks, fee_id=feedback_id)
+
+            Feedbackform.fee_titulo = ultimo_feedback.fee_titulo
+            Feedbackform.fee_anterior = ultimo_feedback.fee_id
+            Feedbackform.fee_acompanhamento = ultimo_feedback.fee_acompanhamento
+
+
+            Feedbackform.save()
+            print(Feedbackform.fee_id)
+            ultimo_feedback.fee_proximo = Feedbackform.fee_id
+            ultimo_feedback.save()
+            messages.success(request, "Feedback de resposta enviado com sucesso!")
+            return redirect('adminFeedbackAll')
+
+        else:
+            form = FeedbacksRespostaForm()
+            form.save()
+            return HttpResponseRedirect('adminRespostaFeedback?submitted=True')
+    else:
+        form = FeedbacksRespostaForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    context['form'] = form
+    context['submitted'] = submitted
+
+    return render(request, 'feedbacks/adminRespostaFeedback.html', context)
+
+def adminDeletarFeedback(request, feedback_id):
+    feedbackInicial = get_object_or_404(Feedbacks, fee_id=feedback_id)
+    list_feedback = Feedbacks.objects.all()
+
+    feedbacks = []
+    f0 = feedbackInicial
+    feedbacks.append(f0)
+    for f1 in list_feedback:
+        if f1.fee_anterior == f0.fee_id:
+            feedbacks.append(f1)
+            f0 = f1
+            if f1.fee_proximo == None:
+                break
+    while feedbacks:
+        f = feedbacks.pop()
+        arquivo_nome = f.fee_arquivo
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        filepath = BASE_DIR + '\\uploads\\' + str(arquivo_nome)
+        f.delete()
+        try:
+            os.remove(filepath)
+        except:
+            pass
+    return redirect('adminFeedbackAll')
 
 #ALUNO-FEEDBACK
 def alunoFeedback(request, user_id):
@@ -104,6 +220,7 @@ def alunoFeedback(request, user_id):
             # handle_uploaded_file(request.FILES["avi_arquivos"])
             Feedbackform.fee_acompanhamento = acompanhamento
             Feedbackform.save()
+            messages.success(request, "Feedback iniciado com sucesso!")
             return redirect('alunoFeedbackAll', user_id)
         else:
             form = FeedbacksForm()
@@ -122,17 +239,16 @@ def alunoFeedbackAll(request, user_id):
 
     user = get_object_or_404(CustomUser, id=user_id)
     aluno = get_object_or_404(AlunoPcd, alu_cpf=user.username)
-    acompanhamento = Acompanhamentos.objects.filter(aco_aluno_pcd = aluno)
-    feedback = Feedbacks.objects.order_by('-fee_id').filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamento.all()], fee_anterior=None )
+    acompanhamentos = Acompanhamentos.objects.filter(aco_aluno_pcd = aluno)
+    feedback = Feedbacks.objects.order_by('-fee_id').filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamentos.all()], fee_anterior=None )
 
     paginator = Paginator(feedback, 7)
     page = request.GET.get('p')
     feedback = paginator.get_page(page)
 
-    return render(request, 'feedbacks/alunofeedbackAll.html', {
+    return render(request, 'feedbacks/alunoFeedbackAll.html', {
         'feedback': feedback,
-        'aluno': aluno,
-        'acompanhamento': acompanhamento,
+        'acompanhamentos': acompanhamentos,
     })
 
 def alunoOpenAllfeedback(request, user_id, feedback_id):
@@ -140,8 +256,8 @@ def alunoOpenAllfeedback(request, user_id, feedback_id):
     user = get_object_or_404(CustomUser, id=user_id)
     aluno = get_object_or_404(AlunoPcd, alu_cpf=user.username)
     feedbackInicial = get_object_or_404(Feedbacks, fee_id=feedback_id)
-    acompanhamento = Acompanhamentos.objects.filter(aco_aluno_pcd = aluno.alu_id)
-    list_feedback = Feedbacks.objects.filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamento.all()])
+    acompanhamentos = Acompanhamentos.objects.filter(aco_aluno_pcd = aluno.alu_id)
+    list_feedback = Feedbacks.objects.filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamentos.all()])
 
     feedback = []
     f0 = feedbackInicial
@@ -151,40 +267,29 @@ def alunoOpenAllfeedback(request, user_id, feedback_id):
         if f1.fee_anterior == f0.fee_id:
             feedback.append(f1)
             f0=f1
-        if f1.fee_proximo == None:
-            ultimo_feedback = f1
+            if f1.fee_proximo == None:
+                ultimo_feedback = f1
+                break
     feedback = reversed(feedback)
     return render(request, 'feedbacks/alunoOpenAllfeedback.html', {
         'feedback': feedback,
-        'aluno': aluno,
-        'acompanhamento': acompanhamento,
         'ultimo_feedback': ultimo_feedback,
         'feedbackInicial': feedbackInicial
     })
 def alunoOpenfeedback(request, user_id, feedback_id ):
-    user = get_object_or_404(CustomUser, id=user_id)
-    aluno = get_object_or_404(AlunoPcd, alu_cpf=user.username)
     feedback = get_object_or_404(Feedbacks, fee_id=feedback_id)
-    acompanhamento = get_object_or_404(Acompanhamentos, aco_id = feedback.fee_acompanhamento.aco_id)
 
     if feedback.fee_proximo != None:
         return alunoOpenAllfeedback(request, user_id, feedback_id)
     return render(request, 'feedbacks/alunoOpenfeedback.html', {
-        'feedback': feedback,
-        'aluno': aluno,
-        'acompanhamento': acompanhamento,
-        'ultimo_feedback': feedback,
+        'feedback': feedback
 
     })
 
-
-
-'''https://django-portuguese.readthedocs.io/en/1.0/topics/forms/modelforms.html'''
 def alunoRespostaFeedback(request, user_id, feedback_id):
     submitted = False
     context = {}
-    user = get_object_or_404(CustomUser, id=user_id)
-    aluno = get_object_or_404(AlunoPcd, alu_cpf=user.username)
+
     if request.POST:
         form = FeedbacksRespostaForm(request.POST, request.FILES)
 
@@ -201,6 +306,7 @@ def alunoRespostaFeedback(request, user_id, feedback_id):
             print(Feedbackform.fee_id)
             ultimo_feedback.fee_proximo = Feedbackform.fee_id
             ultimo_feedback.save()
+            messages.success(request, "Feedback de resposta enviado com sucesso!")
             return redirect('alunoFeedbackAll', user_id)
 
         else:
@@ -214,6 +320,235 @@ def alunoRespostaFeedback(request, user_id, feedback_id):
 
     context['form'] = form
     context['submitted'] = submitted
-    context['aluno'] = aluno
 
     return render(request, 'feedbacks/alunoRespostaFeedback.html', context)
+
+#MONITOR-FEEDBACK
+def monitorFeedback(request, user_id):
+    submitted = False
+    context = {}
+
+    user = get_object_or_404(CustomUser, id=user_id)
+    if request.POST:
+        form = FeedbacksMonitorForm(user, request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Feedback iniciado com sucesso!")
+            return redirect('monitorFeedbackAll', user_id)
+        else:
+            form = FeedbacksMonitorForm(user)
+            form.save()
+            return HttpResponseRedirect('monitorFeedback?submitted=True')
+    else:
+        form = FeedbacksMonitorForm(user)
+        if 'submitted' in request.GET:
+            submitted = True
+    context['form'] = form
+    context['submitted'] = submitted
+
+    return render (request, 'feedbacks/monitorFeedback.html', context)
+
+def monitorFeedbackAll(request, user_id):
+
+    user = get_object_or_404(CustomUser, id=user_id)
+    monitor = get_object_or_404(Monitor, mon_cpf=user.username)
+    monitorias = AcompanhamentoMonitores.objects.filter(AsMon_monitor=monitor)
+    acompanhamentos = Acompanhamentos.objects.filter(aco_id__in=[m.AsMon_acompanhamento.aco_id for m in monitorias.all()])
+    feedback = Feedbacks.objects.order_by('-fee_id').filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamentos.all()], fee_anterior=None )
+
+    paginator = Paginator(feedback, 7)
+    page = request.GET.get('p')
+    feedback = paginator.get_page(page)
+
+    return render(request, 'feedbacks/monitorFeedbackAll.html', {
+        'feedback': feedback,
+        'acompanhamentos': acompanhamentos,
+    })
+
+def monitorOpenAllfeedback(request, user_id, feedback_id):
+
+    user = get_object_or_404(CustomUser, id=user_id)
+    monitor = get_object_or_404(Monitor, mon_cpf=user.username)
+    monitorias = AcompanhamentoMonitores.objects.filter(AsMon_monitor=monitor)
+    feedbackInicial = get_object_or_404(Feedbacks, fee_id=feedback_id)
+    acompanhamentos = Acompanhamentos.objects.filter(aco_id__in=[m.AsMon_acompanhamento.aco_id for m in monitorias.all()])
+    list_feedback = Feedbacks.objects.filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamentos.all()])
+
+    feedback = []
+    f0 = feedbackInicial
+    feedback.append(f0)
+    ultimo_feedback = None
+    for f1 in list_feedback:
+        if f1.fee_anterior == f0.fee_id:
+            feedback.append(f1)
+            f0=f1
+            if f1.fee_proximo == None:
+                ultimo_feedback = f1
+                break
+    feedback = reversed(feedback)
+    return render(request, 'feedbacks/monitorOpenAllfeedback.html', {
+        'feedback': feedback,
+        'ultimo_feedback': ultimo_feedback,
+        'feedbackInicial': feedbackInicial
+    })
+def monitorOpenfeedback(request, user_id, feedback_id):
+    feedback = get_object_or_404(Feedbacks, fee_id=feedback_id)
+
+    if feedback.fee_proximo != None:
+        return monitorOpenAllfeedback(request, user_id, feedback_id)
+    return render(request, 'feedbacks/monitorOpenfeedback.html', {
+        'feedback': feedback,
+
+    })
+
+def monitorRespostaFeedback(request, user_id, feedback_id):
+    submitted = False
+    context = {}
+    if request.POST:
+        form = FeedbacksRespostaForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            Feedbackform = form.save(commit=False)
+            ultimo_feedback = get_object_or_404(Feedbacks, fee_id=feedback_id)
+
+            Feedbackform.fee_titulo = ultimo_feedback.fee_titulo
+            Feedbackform.fee_anterior = ultimo_feedback.fee_id
+            Feedbackform.fee_acompanhamento = ultimo_feedback.fee_acompanhamento
+
+
+            Feedbackform.save()
+            print(Feedbackform.fee_id)
+            ultimo_feedback.fee_proximo = Feedbackform.fee_id
+            ultimo_feedback.save()
+            messages.success(request, "Feedback de resposta enviado com sucesso!")
+            return redirect('monitorFeedbackAll', user_id)
+
+        else:
+            form = FeedbacksRespostaForm()
+            form.save()
+            return HttpResponseRedirect('monitorRespostaFeedback?submitted=True')
+    else:
+        form = FeedbacksRespostaForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    context['form'] = form
+    context['submitted'] = submitted
+
+    return render(request, 'feedbacks/monitorRespostaFeedback.html', context)
+
+#TUTOR-FEEDBACK
+def tutorFeedback(request, user_id):
+    submitted = False
+    context = {}
+
+    user = get_object_or_404(CustomUser, id=user_id)
+    if request.POST:
+        form = FeedbacksTutorForm(user, request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Feedback iniciado com sucesso!")
+            return redirect('tutorFeedbackAll', user_id)
+        else:
+            form = FeedbacksTutorForm(user)
+            form.save()
+            return HttpResponseRedirect('tutorFeedback?submitted=True')
+    else:
+        form = FeedbacksTutorForm(user)
+        if 'submitted' in request.GET:
+            submitted = True
+    context['form'] = form
+    context['submitted'] = submitted
+
+    return render (request, 'feedbacks/tutorFeedback.html', context)
+
+def tutorFeedbackAll(request, user_id):
+
+    user = get_object_or_404(CustomUser, id=user_id)
+    tutor = get_object_or_404(Tutor, tut_cpf=user.username)
+    tutorias = AcompanhamentoTutores.objects.filter(AsTut_tutor=tutor)
+    acompanhamentos = Acompanhamentos.objects.filter(aco_id__in=[t.AsTut_acompanhamento.aco_id for t in tutorias.all()])
+    feedback = Feedbacks.objects.order_by('-fee_id').filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamentos.all()], fee_anterior=None )
+
+    paginator = Paginator(feedback, 7)
+    page = request.GET.get('p')
+    feedback = paginator.get_page(page)
+
+    return render(request, 'feedbacks/tutorFeedbackAll.html', {
+        'feedback': feedback,
+        'acompanhamentos': acompanhamentos,
+    })
+
+def tutorOpenAllfeedback(request, user_id, feedback_id):
+
+    user = get_object_or_404(CustomUser, id=user_id)
+    tutor = get_object_or_404(Tutor, tut_cpf=user.username)
+    tutorias = AcompanhamentoTutores.objects.filter(AsTut_tutor=tutor)
+    feedbackInicial = get_object_or_404(Feedbacks, fee_id=feedback_id)
+    acompanhamentos = Acompanhamentos.objects.filter(aco_id__in=[t.AsTut_acompanhamento.aco_id for t in tutorias.all()])
+    list_feedback = Feedbacks.objects.filter(fee_acompanhamento__in=[a.aco_id for a in acompanhamentos.all()])
+
+    feedback = []
+    f0 = feedbackInicial
+    feedback.append(f0)
+    ultimo_feedback = None
+    for f1 in list_feedback:
+        if f1.fee_anterior == f0.fee_id:
+            feedback.append(f1)
+            f0=f1
+            if f1.fee_proximo == None:
+                ultimo_feedback = f1
+                break
+    feedback = reversed(feedback)
+    return render(request, 'feedbacks/tutorOpenAllfeedback.html', {
+        'feedback': feedback,
+        'ultimo_feedback': ultimo_feedback,
+        'feedbackInicial': feedbackInicial
+    })
+def tutorOpenfeedback(request, user_id, feedback_id):
+    feedback = get_object_or_404(Feedbacks, fee_id=feedback_id)
+
+    if feedback.fee_proximo != None:
+        return tutorOpenAllfeedback(request, user_id, feedback_id)
+    return render(request, 'feedbacks/tutorOpenfeedback.html', {
+        'feedback': feedback,
+
+    })
+
+def tutorRespostaFeedback(request, user_id, feedback_id):
+    submitted = False
+    context = {}
+    if request.POST:
+        form = FeedbacksRespostaForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            Feedbackform = form.save(commit=False)
+            ultimo_feedback = get_object_or_404(Feedbacks, fee_id=feedback_id)
+
+            Feedbackform.fee_titulo = ultimo_feedback.fee_titulo
+            Feedbackform.fee_anterior = ultimo_feedback.fee_id
+            Feedbackform.fee_acompanhamento = ultimo_feedback.fee_acompanhamento
+
+
+            Feedbackform.save()
+            print(Feedbackform.fee_id)
+            ultimo_feedback.fee_proximo = Feedbackform.fee_id
+            ultimo_feedback.save()
+            messages.success(request, "Feedback de resposta enviado com sucesso!")
+            return redirect('tutorFeedbackAll', user_id)
+
+        else:
+            form = FeedbacksRespostaForm()
+            form.save()
+            return HttpResponseRedirect('tutorRespostaFeedback?submitted=True')
+    else:
+        form = FeedbacksRespostaForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    context['form'] = form
+    context['submitted'] = submitted
+
+    return render(request, 'feedbacks/tutorRespostaFeedback.html', context)
